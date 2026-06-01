074c8528f3e40ab3d6eefc22b93f00ad43235a53
Sayuliho <aciongho@gmail.com>
2026-06-01 22:03:13 +0700

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const params = new URLSearchParams(req.query);
+  const url = `https://script.google.com/macros/s/AKfycbycdw7-ZYJaPY5J2varxb82LagiCKAlmDfkLOxCZZYEZwi5ZrpH9GLkZYFX-fg6se2t/exec?${params}`;
+  
+  try {
+    const response = await fetch(url, { redirect: 'follow' });
+    const data = await response.text();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(data);
+  } catch(e) {
+    res.status(500).json({ok: false, msg: e.toString()});
+  }
+}
