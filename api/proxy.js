cca6810ea304d9c024ae5bfdee210d35bc629bea
Sayuliho <aciongho@gmail.com>
2026-06-01 22:15:20 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,20 +1,21 @@
 export default async function handler(req, res) {
+  const { action, ...rest } = req.query;
+  
+  let url = `https://script.google.com/macros/s/AKfycbycdw7-ZYJaPY5J2varxb82LagiCKAlmDfkLOxCZZYEZwi5ZrpH9GLkZYFX-fg6se2t/exec`;
+  
   const params = new URLSearchParams(req.query);
-  const url = `https://script.google.com/macros/s/AKfycbycdw7-ZYJaPY5J2varxb82LagiCKAlmDfkLOxCZZYEZwi5ZrpH9GLkZYFX-fg6se2t/exec?${params}`;
+  url = url + '?' + params.toString();
   
   try {
     const response = await fetch(url, {
       method: 'GET',
       redirect: 'follow',
-      headers: {
-        'Cache-Control': 'no-cache',
-        'Pragma': 'no-cache'
-      }
+      headers: { 'Cache-Control': 'no-cache' }
     });
     const data = await response.text();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-store');
+    res.setHeader('Cache-Control', 'no-store, no-cache');
     res.status(200).send(data);
   } catch(e) {
     res.status(500).json({ok: false, msg: e.toString()});
